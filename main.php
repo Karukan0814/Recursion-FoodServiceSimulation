bd1f7b50a7de48071b1f44337f95196c2ca30acc
Karukan0814 <46077409+Karukan0814@users.noreply.github.com>
2024-02-24 16:51:44 -0800

    テストコード

--- main.php
+++ main.php
@@ -1,205 +1,43 @@
+
 <?php
-abstract class Person {
-    protected string $name;
-    protected int $age;
-    protected string $address;
 
-
-
-    public function __construct(string $name,int $age,string $address) {
-        $this->name = $name;
-        $this->age = $age;
-        $this->address = $address;
-
+spl_autoload_extensions(".php");
+spl_autoload_register(function ($class){
+    $base_dir = __DIR__. '/src/';
+    $file = $base_dir.str_replace('\\', '/', $class).'.php';
+    if(file_exists(($file))){
+        require $file;
     }
+});
 
-}
 
 
-class Customer extends Person  {
-    protected string $interestedCategory;
 
-    public function __construct(string $name,int $age,string $address,array $interestedCategory) {
-        parent::__construct($name,$age,$address);
-        $this->interestedCategory = $interestedCategory;
+// レストランの saizeriya は、Chef の Invah、Cashier の Nadia を雇っています。
+// saizeriya のメニューは CheesBurger、Fettuccine、HwaiianPizza、Spaghetti があります。
+// 客である Tom は、Margherita x 1, CheseeBurger x 2, Spaghetti x 1 を食べたく saizeriya でオーダーしました。
 
-    }
+$Invah= new Persons\Employees\Chef("Invah",40,"Invah's address",1,100);
+$Nadia= new Persons\Employees\Cashier("Nadia",30,"Nadia's address",2,200);
+$CheeseBurger=new FoodItems\CheeseBurger(500.5,"oishii cheese");
+$Fettuccine=new FoodItems\Fettuccine(300,"special");
+$HawaiianPizza= new FoodItems\HawaiianPizza(200,"originally from waikiki");
+$Spaghetti= new FoodItems\Spaghetti(600,"we can serve all kinds of spaghetti");
 
-    public function order(Restaurant $restaurant,array $category): Invoice {
-        return new Invoice(0,0,0);
-    }
 
-}
 
+$saizeriya = new Restaurants\Restaurant([$CheeseBurger,$Fettuccine,$HawaiianPizza,$Spaghetti],[$Invah,$Nadia]);
 
-class Employee extends Person  {
-    protected int $employeeId;
-    protected float $salary;
 
+// 顧客の食べたい　食べ物の名前＝＞個数　のマップ
+$interestedTasteMap=[
 
-    public function __construct(string $name,int $age,string $address,int $employeeId,float $salary) {
-        parent::__construct($name,$age,$address);
-        $this->employeeId = $employeeId;
-        $this->salary = $salary;
+    "Margherita"=> 1,
+    "CheseeBurger"=> 2,
+    "Spaghetti"=> 1,
 
+];
 
-    }
-
-}
-class Chef extends Employee  {
-    protected int $employeeId;
-    protected float $salary;
-
-
-    public function __construct(string $name,int $age,string $address,int $employeeId,float $salary) {
-        parent::__construct($name,$age,$address,$employeeId,$salary);
-    }
-
-
-    public function prepareFood(FoodOrder $foodOrder): string {
-        return "prepare Food";
-    }
-
-}
-
-
-class Cashier extends Employee  {
-    protected int $employeeId;
-    protected float $salary;
-
-
-    public function __construct(string $name,int $age,string $address,int $employeeId,float $salary) {
-        parent::__construct($name,$age,$address,$employeeId,$salary);
-    }
-
-
-    public function generateOrder(array $categories,Restaurant $restaurant): FoodOrder {
-        return new FoodOrder([],0);
-    }
-    
-    public function generateInvoice(FoodOrder $foodorder): Invoice {
-        return new Invoice(0,0,0);
-    }
-}
-
-
-class Restaurant  {
-    protected array $menu;
-    protected array $employees;
-
-
-    public function __construct(array $menu,array $employees) {
-        $this->menu = $menu;
-        $this->employees = $employees;
-
-    }
-
-
-    public function order(array $categories): Invoice {
-        return new Invoice(0,0,0);
-    }
-    
-}
-
-
-
-abstract class FoodItem {
-    protected string $name;
-    protected float $price;
-    protected string $description;
-
-
-
-    public function __construct(string $name,float $price,string $description) {
-        $this->name = $name;
-        $this->price = $price;
-        $this->description = $description;
-
-    }
-
-    abstract public static function getCategory():string;
-
-}
-
-
-class HawaiianPizza extends FoodItem  {
-    const CATEGORY = self::class;
-
-    public function __construct(string $name,float $price,string $description) {
-        parent::__construct($name,$price,$description);
-
-    }
-    public static function getCategory(): string {
-        return self::CATEGORY;
-    }
-}
-
-class Spaghetti extends FoodItem  {
-
-    const CATEGORY = self::class;
-
-    public function __construct(string $name,float $price,string $description) {
-        parent::__construct($name,$price,$description);
-
-    }
-    public static function getCategory(): string {
-        return self::CATEGORY;
-    }
-}
-class CheeseBurger extends FoodItem  {
-
-    const CATEGORY = self::class;
-
-    public function __construct(string $name,float $price,string $description) {
-        parent::__construct($name,$price,$description);
-
-    }
-    public static function getCategory(): string {
-        return self::CATEGORY;
-    }
-
-}
-class Fettuccine extends FoodItem  {
-
-    const CATEGORY = self::class;
-
-    public function __construct(string $name,float $price,string $description) {
-        parent::__construct($name,$price,$description);
-
-    }
-    public static function getCategory(): string {
-        return self::CATEGORY;
-    }
-
-}
-
-class FoodOrder   {
-    protected array $items;
-    protected int $orderTime;
-
-    public function __construct(array $items,int $orderTime) {
-        $this->items = $items;
-        $this->orderTime = $orderTime;
-
-
-
-    }
-}
-
-class Invoice   {
-    protected float $finalPrice;
-    protected int $orderTime;
-
-    protected int $estimatedTimeInMinute;
-
-    public function __construct(float $finalPrice,int $orderTime,int $estimatedTimeInMinute) {
-        $this->finalPrice = $finalPrice;
-        $this->orderTime = $orderTime;
-        $this->estimatedTimeInMinute = $estimatedTimeInMinute;
-
-        
-
-
-
-    }
-}
+$Tom=new Persons\Customers\Customer("Tom",33,"Tom's address is in W.C.",$interestedTasteMap);
+$invoice=$Tom->order($saizeriya);
+$invoice->printInvoice();
